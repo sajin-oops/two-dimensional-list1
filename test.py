076781725347f601e8250c74bc9164f8862df4fd
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-08-06 21:09:00 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,11 @@
+fruits = ["apple","mango","orange"]
+vegetables = ["carrot","potato","onion"]
+meats = ["fish","chicken","beaf"]
+
+groceories = [fruits,vegetables,meats]
+
+print(groceories)
+
+'''
+[['apple', 'mango', 'orange'], ['carrot', 'potato', 'onion'], ['fish', 'chicken', 'beaf']]
+'''
